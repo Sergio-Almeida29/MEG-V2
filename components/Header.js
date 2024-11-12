4223d88f94cccaff91c0859e63bc4b378735a8b2
SergioAlmeida5 <tnpolldoge@gmail.com>
2024-11-12 12:21:25 +0000

    Header v1.2 FRAC

--- components/Header.js
+++ components/Header.js
@@ -12,29 +12,34 @@ export default function Header() {
     <header className={styles.header}>
 
       <div className={styles.logoContainer}>
-        <a href="/" ><img src="/logo.png" alt="FRAC" className={styles.logo}/></a>
+        <a href="/"><img src="/logo.png" alt="FRAC" className={styles.logo} /></a>
       </div>
 
-      <div className={styles.titleContainer}><h1>Frente Revolucionária de Aprendizes e Caloiros</h1></div>
-
-      <div className={styles.menuContainer} onClick={toggleMenu}>
-        ☰
+      <div className={styles.titleContainer}>
+        <h1>Frente Revolucionária de Aprendizes e Caloiros</h1>
       </div>
 
-      {/* Apply conditional rendering for the dropdown menu */}
-      {isMenuOpen && (
-        <div className={styles.dropdownMenu}>
-          <ul>
-            <li><a href="/historia">História</a></li>
-            <li><a href="/atualmente">Atualmente</a></li>
-            <li><a href="/antigos_caloiros">Antigos Caloiros</a></li>
-            <li><a href="/premios">Prémios</a></li>
-            <li><a href="/ficheiros">Ficheiros</a></li>
-            <li><a href="/discografia">Discografia</a></li>
-          </ul>
-        </div>
-      )}
+      {/* Rotating menu icon */}
+      <div 
+        className={`${styles.menuContainer} ${isMenuOpen ? styles.rotate : ''}`} 
+        onClick={toggleMenu}
+      >
+        <div className={styles.menuLine}></div>
+        <div className={styles.menuLine}></div>
+        <div className={styles.menuLine}></div>
+      </div>
 
+      <div className={`${styles.dropdownMenu} ${isMenuOpen ? styles.open : ''}`}>
+        <ul>
+          <li><a href="/historia">História</a></li>
+          <li><a href="/atualmente">Atualmente</a></li>
+          <li><a href="/antigos_caloiros">Antigos Caloiros</a></li>
+          <li><a href="/premios">Prémios</a></li>
+          <li><a href="/ficheiros">Ficheiros</a></li>
+          <li><a href="/discografia">Discografia</a></li>
+        </ul>
+      </div>
+      
     </header>
   );
 }
